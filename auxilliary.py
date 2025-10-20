5af5525253b2d0257741e16780ea5844db573cf9
marekrams <marek.rams@uj.edu.pl>
2025-10-20 16:30:22 +0200

    upgrades

--- auxilliary.py
+++ auxilliary.py
@@ -11,37 +11,37 @@ def op1site(op, site, s2i, qI, dI):
     return mps.product_mpo(oo)
 
 
-def merge_mpo(O, s2i, merge=True):
+def merge_sites(O, s2i, merge=True):
     if not merge:
         return O
-    ii = sorted([s2i['D1'], s2i['S1'], s2i['S2'], s2i['S3'], s2i['S4']])
+
+    ii = sorted([v for k, v in s2i.items() if k[0] in 'DS'])  # try to merge 'DS' sites
     mi = min(ii)
+    if ii != list(range(mi, mi + len(ii))):  # try to merge 'S' sites
+        ii = sorted([v for k, v in s2i.items() if k[0] in 'S'])
+        mi = min(ii)
+        if ii != list(range(mi, mi + len(ii))):
+            return O
 
-    assert ii == list(range(mi, mi+5)), "Sites should be neighbouring"
-    Onew = mps.Mpo(N=len(O) - 4)
+    M = len(ii)
+    Onew = mps.MpsMpoOBC(N=len(O) - M + 1, nr_phys=O.nr_phys)
 
-    for ii in range(mi):
-        Onew[ii] = O[ii]
-    for ii in range(mi + 5, len(O)):
-        Onew[ii - 4] = O[ii]
-    X = yastn.ncon([O[mi], O[mi + 1], O[mi + 2], O[mi + 3], O[mi + 4]], [[-0, -1, 1, -2], [1, -3, 2, -4], [2, -5, 3, -6], [3, -7, 4, -8], [4, -9, -10, -11]])
-    Onew[mi] = yastn.fuse_legs(X, axes=(0, (1, 3, 5, 7, 9), 10, (2, 4, 6, 8, 11)))
-    return Onew
+    for i in range(mi):
+        Onew[i] = O[i]
+    for i in range(mi + M, len(O)):
+        Onew[i - M + 1] = O[i]
 
+    tensors = [O[i] for i in ii]
 
-def merge_mps(O, s2i, merge=True):
-    if not merge:
-        return O
-    ii = sorted([s2i['D1'], s2i['S1'], s2i['S2'], s2i['S3'], s2i['S4']])
-    mi = min(ii)
+    if O.nr_phys == 2:
+        inds = [[i, -2 * i - 1, i + 1, -2 * i - 2] for i in range(M)]
+        inds[-1][-2] = -2 * M - 1
+        axes = (0, [2*i+1 for i in range(M)], 2*M+1, [2*i+2 for i in range(M)])
+    elif O.nr_phys == 1:
+        inds = [[i, - i - 1, i + 1] for i in range(M)]
+        inds[-1][-1] = - M - 1
+        axes = (0, [i+1 for i in range(M)], M+1)
 
-    assert ii == list(range(mi, mi+5)), "Sites should be neighbouring"
-    Onew = mps.Mps(N=len(O) - 4)
-
-    for ii in range(mi):
-        Onew[ii] = O[ii]
-    for ii in range(mi + 5, len(O)):
-        Onew[ii - 4] = O[ii]
-    X = yastn.ncon([O[mi], O[mi + 1], O[mi + 2], O[mi + 3], O[mi + 4]], [[-0, -1, 1], [1, -2, 2], [2, -3, 3], [3, -4, 4], [4, -5, -6]])
-    Onew[mi] = yastn.fuse_legs(X, axes=(0, (1, 2, 3, 4, 5), 6))
+    X = yastn.ncon(tensors, inds)
+    Onew[mi] = yastn.fuse_legs(X, axes=axes)
     return Onew
--- hamiltonians.py
+++ hamiltonians.py
@@ -38,7 +38,7 @@ def local_operators(sym='U1'):
     return qI, qc, qcp, qn, dx, dn1, dn2, dI, m12, m21
 
 
-def Hamiltonian_dpt_4U_position(NW, muL, muR, muDs, vS, U, w0=1, order='DLSR', sym='U1'):
+def Hamiltonian_dpt_position(NW, NS, muL, muR, muDs, vS, U, w0=1, order='DLSR', sym='U1'):
     """ generate MPO for DPT in position basis. first dot is interacting with 4 sites """
     #
     qI, qc, qcp, qn, dx, dn1, dn2, dI, m12, m21 = local_operators(sym=sym)
@@ -47,17 +47,17 @@ def Hamiltonian_dpt_4U_position(NW, muL, muR, muDs, vS, U, w0=1, order='DLSR', s
     vL = vR = vLR = w0
     #
     if order == 'DLSR':
-        sites = [D(1)]  # will have SLR geomery
+        sites = [D(1)]
         sites += [L(k) for k in range(NW, 0, -1)]  # 'L1' is for L mode at the junction
         sites += [S(k) for k in range(1, NS + 1)]  # 'S1' connected to L1
         sites += [R(k) for k in range(1, NW + 1)]  # 'R1' is for R mode at the junction
     elif order == 'LDSR':
-        sites = [L(k) for k in range(NW, 0, -1)]  # 'L1' is for L mode at the junction
+        sites = [L(k) for k in range(NW, 0, -1)]   # 'L1' is for L mode at the junction
         sites += [S(1), S(2), D(1), S(3), S(4)]
         sites += [R(k) for k in range(1, NW + 1)]  # 'R1' is for R mode at the junction
-    sites = order
+    else:
+        sites = order
 
-    # here sites are ordered in position
     s2i = {s: i for i, s in enumerate(sites)}
     i2s = {i: s for i, s in enumerate(sites)}
     II = mps.product_mpo([dI if 'D' in site else qI for site in sites])
@@ -68,14 +68,15 @@ def Hamiltonian_dpt_4U_position(NW, muL, muR, muDs, vS, U, w0=1, order='DLSR', s
     except TypeError:
         muDs = [muDs] * 2
 
-    muSs = [muL, muL, muR, muR]
-
     terms = []
     for k in range(1, NW + 1): # on-site energies
         terms.append((muL, [L(k)], [qn]))
         terms.append((muR, [R(k)], [qn]))
-    for k, ms in enumerate(muSs, start=1): # on-site energies
-        terms.append((ms, [S(k)], [qn]))
+
+    for k in range(1, NS // 2 + 1): # on-site energies
+        terms.append((muL, [S(k)], [qn]))
+    for k in range(NS // 2 + 1, NS + 1): # on-site energies
+        terms.append((muR, [S(k)], [qn]))
 
     terms.append((muDs[0], [D(1)], [dn1]))
     terms.append((muDs[1], [D(1)], [dn2]))
@@ -103,24 +104,22 @@ def Hamiltonian_dpt_4U_position(NW, muL, muR, muDs, vS, U, w0=1, order='DLSR', s
     return H, s2i, i2s
 
 
-def Hamiltonian_dpt_4U_momentum(NW, muL, muR, muDs, vS, U, w0=1, order='DLR', sym='U1'):
+def Hamiltonian_dpt_momentum(NW, NS, muL, muR, muDs, vS, U, w0=1, order='DLR', sym='U1'):
     """ generate mpo for dpt model in mixed basis """
-
+    #
     qI, qc, qcp, qn, dx, dn1, dn2, dI, m12, m21 = local_operators(sym=sym)
-    NS = 4
     #
     vL = vR = vLR = w0
     #
     NW1 = NW + NS // 2 + 1
     #
     if order == 'DLR':
-        sites = [D(1)]  # will have SLR geomery
-        sites += [L(k) for k in range(1, NW1)]  # 'L1' is for L mode at the junction
-        sites += [R(k) for k in range(1, NW1)]  # 'R1' is for R mode at the junction
+        sites = [D(1)]
+        sites += [L(k) for k in range(1, NW1)]
+        sites += [R(k) for k in range(1, NW1)]
     else:
         sites = order
 
-    # here sites are ordered in position
     s2i = {s: i for i, s in enumerate(sites)}
     i2s = {i: s for i, s in enumerate(sites)}
     II = mps.product_mpo([dI if 'D' in site else qI for site in sites])
@@ -130,8 +129,6 @@ def Hamiltonian_dpt_4U_momentum(NW, muL, muR, muDs, vS, U, w0=1, order='DLR', sy
         assert lD == 2
     except TypeError:
         muDs = [muDs] * 2
-
-    muSs = [muL, muL, muR, muR]
 
     terms = []
 
@@ -151,13 +148,11 @@ def Hamiltonian_dpt_4U_momentum(NW, muL, muR, muDs, vS, U, w0=1, order='DLR', sy
 
     for k1 in range(1, NW1):
        for k2 in range(1, NW1):
-            amp = U * (2 / NW1) * np.sin(np.pi * 1 * k1 / NW1) * np.sin(np.pi * 1 * k2 / NW1)
-            terms.append((amp, [D(1), L(k1), L(k2)], [dn1 - dI / 2, qcp, qc]))
-            terms.append((amp, [D(1), R(k1), R(k2)], [dn1 - dI / 2, qcp, qc]))
-            amp = U * (2 / NW1) * np.sin(np.pi * 2 * k1 / NW1) * np.sin(np.pi * 2 * k2 / NW1)
-            terms.append((amp, [D(1), L(k1), L(k2)], [dn1 - dI / 2, qcp, qc]))
-            terms.append((amp, [D(1), R(k1), R(k2)], [dn1 - dI / 2, qcp, qc]))
-    terms.append((-4 * U / 2, [D(1)], [dn1 - dI / 2]))
+           for ii in range(1, NS // 2 + 1):
+                amp = U * (2 / NW1) * np.sin(np.pi * ii * k1 / NW1) * np.sin(np.pi * ii * k2 / NW1)
+                terms.append((amp, [D(1), L(k1), L(k2)], [dn1 - dI / 2, qcp, qc]))
+                terms.append((amp, [D(1), R(k1), R(k2)], [dn1 - dI / 2, qcp, qc]))
+    terms.append((-NS * U / 2, [D(1)], [dn1 - dI / 2]))
 
     Hterms = [mps.Hterm(v, tuple(s2i[x] for x in p), o) for v, p, o in terms]
     H = mps.generate_mpo(II, Hterms)
@@ -165,11 +160,10 @@ def Hamiltonian_dpt_4U_momentum(NW, muL, muR, muDs, vS, U, w0=1, order='DLR', sy
 
 
 
-def Hamiltonian_dpt_4U_mixed(NW, muL, muR, muDs, vS, U, w0=1, order='LRDSLR', sym='U1'):
+def Hamiltonian_dpt_mixed(NW, NS, muL, muR, muDs, vS, U, w0=1, order='LRDSLR', sym='U1'):
     """ generate mpo for dpt model in mixed basis """
 
     qI, qc, qcp, qn, dx, dn1, dn2, dI, m12, m21 = local_operators(sym=sym)
-    NS = 4
     #
     vL = vR = vLR = w0
     #
@@ -185,14 +179,14 @@ def Hamiltonian_dpt_4U_mixed(NW, muL, muR, muDs, vS, U, w0=1, order='LRDSLR', sy
         for k in range(1, NW1):
             sites.append(L(k))
             sites.append(R(k))
-        sites = sites[:NW1] + ['S1', 'S2', 'D1', 'S3', 'S4'] + sites[NW1:]
-
+        ss = [S(k) for k in range(1, NS + 1)]
+        sites = sites[:NW1] + ss[:NS//2] + ['D1'] + ss[NS//2:] + sites[NW1:]
     elif order == 'DLRSLR':
         sites = []
         for k in range(1, NW1):
             sites.append(L(k))
             sites.append(R(k))
-        sites = [D(1)] + sites[:NW1] + ['S1', 'S2', 'S3', 'S4'] + sites[NW1:]
+        sites = [D(1)] + sites[:NW1] + [S(k) for k in range(1, NS + 1)] + sites[NW1:]
     else:
         sites = order
 
@@ -207,15 +201,14 @@ def Hamiltonian_dpt_4U_mixed(NW, muL, muR, muDs, vS, U, w0=1, order='LRDSLR', sy
     except TypeError:
         muDs = [muDs] * 2
 
-    muSs = [muL, muL, muR, muR]
-
     terms = []
     for k in range(1, NW1):  # on-site energies
         terms.append((muL + 2 * vL * np.cos(np.pi * k / NW1), [L(k)], [qn]))
         terms.append((muR + 2 * vR * np.cos(np.pi * k / NW1), [R(k)], [qn]))
-
-    for k, ms in enumerate(muSs, start=1): # on-site energies
-        terms.append((ms, [S(k)], [qn]))
+    for k in range(1, NS // 2 + 1): # on-site energies
+        terms.append((muL, [S(k)], [qn]))
+    for k in range(NS // 2 + 1, NS + 1): # on-site energies
+        terms.append((muR, [S(k)], [qn]))
 
     terms.append((muDs[0], [D(1)], [dn1]))
     terms.append((muDs[1], [D(1)], [dn2]))
