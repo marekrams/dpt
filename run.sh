0096c843b51a21bf662b96075829a8ef51344eff
keyi-ray-liu <>
2026-02-11 22:57:17 -0500

    run script

--- run.sh
+++ run.sh
@@ -2,15 +2,17 @@
 
 c=$(date +%s)
 TH=1
+name="vs0.75"
+descp="U*"
 #TH=1
 d=$(pwd)
-for f in $(ls -d U*)
+for f in $(ls -d ${descp})
 do
 	let c=c+1
 	full=${d}/$f
 	echo $full
 	cp ~/Desktop/github/dpt/*py $full
-	tmux new-session -d -s "$c" -c "$full" \
+	tmux new-session -d -s "$c TH$TH $name" -c "$full" \
 	 "OMP_NUM_THREADS=$TH MKL_NUM_THREADS=$TH OPENBLAS_NUM_THREADS=$TH BLIS_NUM_THREADS=$TH VECLIB_MAXIMUM_THREADS=$TH NUMEXPR_NUM_THREADS=$TH \
 	python submittable.py &> log"
 	echo $c
